f3226839690835edb56abc3a316206b073aff882
Brandlee97 <Brandon.peralez02@utrgv.edu>
2023-11-16 12:26:38 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/library')
+def user(name):
+    return f"hello {name}"
+
+
+@app.route('/new')
+def admin():
+    return redirect(url_for('user', name='Admin!'))
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
