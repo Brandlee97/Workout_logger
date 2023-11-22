bbb3b226f3a4aae451d1f4c79db97f6d52e5b52e
Brandlee97 <Brandon.peralez02@utrgv.edu>
2023-11-22 09:54:01 -0600

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -23,7 +23,6 @@ def create():
 @app.route('/logs')
 def logs():
     return render_template("logs.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
