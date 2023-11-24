6c285b2ec1bc65e5bdb6ed6c1d52310fd62c217a
Brandlee97 <Brandon.peralez02@utrgv.edu>
2023-11-24 00:09:57 -0600

    added different things to help the project

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template
-import requests
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
-from sqllalchemy.ext.declarative import declarative_base
-from swlalchemy.orm import sessionmaker
+from flask import Flask, render_template, request, redirect, url_for, flash
+from database import db, Workout
 
 
 app = Flask(__name__)
@@ -11,6 +8,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout_database.db'
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+    
 # Home/About Page   **Done/Completed
 @app.route('/')
 def homepage():
@@ -20,19 +20,65 @@ def homepage():
 def login():
     return render_template("login.html")
 
-# Create a new workout
-@app.route('/create_workout', methods=[''])
+@app.route('/workout_form')
+def workout_form():
+    return render_template("workout_form.html")
+
+# Create a submit new workout
+@app.route('/saved_workout', methods=['GET', 'POST'])
 def create():
-    if request
-    return render_template("create.html")
+    if request.method == 'POST':
+        muscle_group = request.form['muscle_group']
+        exercise = request.form['exercise']
+        sets = int(request.form['sets'])
+        reps = int(request.form['reps'])
+
+        created_workout = Workout(muscle_group=muscle_group, exercise=exercise, sets=sets, reps=reps)
+        db.session.add(created_workout)
+        db.session.commit()
+    return redirect(url_for('workout_logs'))
+    
 
 # Look at past workouts
-@app.route('/logs')
-def logs():
-    return render_template("logs.html")
+@app.route('/logged_workouts')
+def workout_logs():
+    workouts = Workout.query.all()  
+    return render_template("logged_workouts.html", workouts=workouts)
 
 @app.route('/About_Us')
 def aboutUs():
-    return render_template("aboutUs.html")
+    return render_template("aboutUs.html")\
+    
+    
+# Deleting and editing logs
+@app.route('/delete/<int>:workout_id>', methods=['POST'])
+def delete_exercise(workout_id):
+    workout = Workout.query.get(workout_id)
+    db.session.delete(workout)
+    db.session.commit()
+    flash("Exercise deleted")
+    return redirect(url_for('workout_logs'))
+
+@app.route('/edit/<int:workout_id>', methods=['GET', 'POST'])
+def edit_exercise(workout_id):
+    workout = Workout.query.get(workout_id)
+    if workout:
+        if request.method == 'POST':
+            workout.muscle_group = request.form['muscle_group']
+            workout.exercise = request.form['exercise']
+            workout.sets = int(request.form['sets'])
+            workout.reps = int(request.form['reps'])
+            
+            db.session.commit()
+
+            flash("Exercise Edit Successful")
+            return redirect(url_for('workout_logs'))
+        else:
+            return render_template('edit_workout.html', workout=workout)
+    else:
+        flash('Workout not found!')
+        return redirect(url_for('logged_workouts'))
+    
+
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -2,12 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class wourlout(db.model):
+class Workout(db.Model):
    id = db.Column(db.Integer, primary_key=True)
 
-    muscle_group = db.Column(db.String(100), nullable=False)
-    exercise = db.Column(db.string(100), nullable=False)
-    sets = db.Column(db.Integer, nullable=False)
-    reps = db.Column(db.Integer, nullable=False)
+   muscle_group = db.Column(db.String(100), nullable=False)
+   exercise = db.Column(db.String(100), nullable=False)
+   sets = db.Column(db.Integer, nullable=False)
+   reps = db.Column(db.Integer, nullable=False)
 
     
