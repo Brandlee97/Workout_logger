71d2e54f5bb4dc58ba4ee0f45c9dd16825232eae
alan2435 <alan.garciagarcia01@utrgv.edu>
2023-11-26 16:00:06 -0600

    Homepage Completed #2

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, redirect, url_for, render_template
-
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # SQLALchemy database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///logindb.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    username = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100))
+
+# Create database
+with app.app_context():
+    db.create_all()
+
 
 # Home/About Page   **Done/Completed
 @app.route('/')
 def homepage():
     return render_template("home.html")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
 # Create a new workout
 @app.route('/create_workout')
